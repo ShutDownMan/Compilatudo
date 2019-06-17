cd40416472ea555262fbe59454f50404018695d5
ShutDownMan <jedson_gabriel@hotmail.com>
2019-06-17 17:53:18 -0300

    non

--- CCompiler/entrada.c
+++ CCompiler/entrada.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 
-int inteiro = 0;
+int inteiro = 0, x = 2;
+
+erro1
 
 int func(int a, int b, int c) {
     return a + b + c;
 }
+
+erro2
 
 int main() {
     int novo_int = func(1, 2, 3);
 
     float floating = 1.2 + 2.3;
 
-    novo_int = 12 && 15;
+    novo_int = 12 && 0;
 
     1 + 1;
 
-    printf("%d\n", (novo_int++));
+    2/printf("%d\n", (novo_int++));
 
     if(95 < 46) {
         while((novo_int < (523.1 + 15)/2) && func(3, inteiro, 1)) {
